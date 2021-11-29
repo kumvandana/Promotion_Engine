f682bd814016e63ec76784499820866f8f061c74
VANDANA KUMARI <kumari.vandana04@gmail.com>
2021-11-29 11:21:37 +0530

    Adding initial configuration, pom and main method

---
+++ src/test/java/com/self/promotion/engine/Promotion_EngineTest.java
@@ -0,0 +1,19 @@
+package com.self.promotion.engine;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.io.IOException;
+
+@Slf4j
+public class Promotion_EngineTest {
+
+	Promotion_Engine promotion_engine = new Promotion_Engine();
+
+	@Test
+	public void testScenario1() throws IOException {
+
+		//Add the test inputs and try
+//		promotion_engine.main(new String[0]);
+	};
+}
